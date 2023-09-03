36e89feb1f936c54d76c6fc50b0834ac3d50411c
NVinh02 <80191765+NVinh02@users.noreply.github.com>
2023-09-04 00:09:41 +0700

    fix: re-add exception
    Re-add unauthorized exception and handled exception

---
+++ LostAndFound.Core/Exceptions/Authenticate/UnauthorizedException.cs
@@ -0,0 +1,11 @@
+﻿using LostAndFound.Core.Exceptions.Common;
+
+namespace LostAndFound.Core.Exceptions.Authenticate
+{
+    public class UnauthorizedException : HandledException
+    {
+        public UnauthorizedException() : base(401, "You are not authorized to use this")
+        {
+        }
+    }
+}
